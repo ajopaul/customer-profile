1e066784679c373684789ff88bc6daf4c3f7b8a3
Required For Git Stash When Using Git Duet <stash@example.com>
2019-03-04 19:16:03 +1100

    Add Swagger UI Integration

--- src/main/java/com/ajopaul/qantas/customerprofile/CustomerProfileController.java
+++ src/main/java/com/ajopaul/qantas/customerprofile/CustomerProfileController.java
@@ -1,14 +1,23 @@
 package com.ajopaul.qantas.customerprofile;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CustomerProfileController {
+
+    @Autowired
+    private CustomerProfileService customerProfileService;
+
+    @GetMapping("/customers")
+    public List<Customer> fetchAllCustomers() {
+        return customerProfileService.getAllCustomers();
+    }
 
 /*
     @GetMapping("/customers")
